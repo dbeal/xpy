5cd28397fbae6ac2ce927b1bf9b6de57e4a0a4c6
David Beal <david.beal@gmail.com>
2017-06-14 17:13:16 -0400

    more streamlining

--- xpy/__main__.py
+++ xpy/__main__.py
@@ -1,18 +1,6 @@
 
 import sys
-
 from .Main import Main
 
-class Foo(object):
-    @classmethod
-    def test(self):
-        print('test')
-
-
-def main():
-    print('there')
-    return Main.main()
-
 if __name__ == '__main__':
-
-    sys.exit(main())
+    sys.exit(Main.main())
