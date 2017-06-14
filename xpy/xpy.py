329bf799c4c42cc4bca04dadef4560c7d67b0aa0
David Beal <david.beal@gmail.com>
2017-06-14 15:51:39 -0400

    vim

---
+++ xpy/xpy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+class XPY(object):
+    @classmethod
+    def run(self):
+        ic = code.InteractiveConsole()
+        ic.interact()
+
+if __name__ == '__main__':
+    XPY.run()
